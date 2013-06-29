14a5bc8e3f54c6398ee3060e122435bbf3d36873
wardpenney <ward.penney@gmail.com>
2013-06-28 21:48:03 -0400

    Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PivotalBlogExampleFontcustom::Application.config.secret_key_base = 'beb97f6210ba5d728b8569f7b17ba6d56889e4decda66596ec947c8f46d556a682fe1f6122908af60e085308165c68beb877497145682443e90e08ecaad79436'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PivotalBlogExampleFontcustom::Application.config.session_store :cookie_store, key: '_pivotal-blog-example-fontcustom_session'
